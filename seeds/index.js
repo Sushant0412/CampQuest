0a39993e6d7584e1e5985cfecb8be85c6be9182d
Sushant0412 <mikesushant05@gmail.com>
2024-06-02 16:53:29 +0530

    fixed seeds

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import cities from "./cities.js";
 import { places, descriptors } from "./seedHelpers.js";
 import Campground from "../models/campground.js";
-import axios from "axios";
 
 mongoose
   .connect("mongodb://localhost:27017/CampQuest")
@@ -27,22 +26,10 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     try {
-      const response = await axios.get(
-        "https://api.unsplash.com/photos/random",
-        {
-          params: {
-            query: "nature",
-            client_id: "tYmAbv6yEGh2xDZg6-3YPupYIrY4tkACDQGTIg9AoL4",
-            w: 800,
-            h: 600,
-          },
-        }
-      );
-      const imageUrl = response.data.urls.regular;
       const camp = new Campground({
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
         title: `${sample(descriptors)} ${sample(places)}`,
-        image: imageUrl,
+        image: "https://source.unsplash.com/collection/483251",
         author: "6659aa8a0deb0a17a0920703",
         description:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
