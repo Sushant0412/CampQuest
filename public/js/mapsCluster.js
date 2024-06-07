af463c26aca68062f1176f81d35e62a123e8fa07
Sushant0412 <mikesushant05@gmail.com>
2024-06-07 16:35:16 +0530

    add css to home page login.rigister and added map controls

--- public/js/mapsCluster.js
+++ public/js/mapsCluster.js
@@ -1,11 +1,19 @@
 mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/streets-v12",
   center: [-103.5917, 40.6699],
   zoom: 3.5,
 });
+
+const nav = new mapboxgl.NavigationControl({
+  showCompass: true, // Include the compass button
+  showZoom: true,    // Include the zoom-in and zoom-out buttons
+  visualizePitch: true // Visualize pitch by rotating X-axis of compass
+});
+
+map.addControl(nav, 'top-right');
 
 map.on("load", () => {
   // Add a new source from our GeoJSON data and
--- public/js/showMap.js
+++ public/js/showMap.js
@@ -6,6 +6,8 @@ const map = new mapboxgl.Map({
   zoom: 10,
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
   .setLngLat(campgrounds.geometry.coordinates)
   .setPopup(
